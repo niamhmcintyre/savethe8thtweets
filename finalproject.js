e5a7d97053d56616d7aef61be15ec42b05d2ba1d
niamhmcintyre <niamhmcintyre64@gmail.com>
2017-04-18 15:38:23 +0100

    Add files via upload

---
+++ finalproject.js
@@ -0,0 +1,57 @@
+
+				var showCSTemplates = function(sel) {   
+		  var locations = [ 
+		    "", 
+		            
+		" Catholic was the top mention, featuring in 908 bios",
+		"God was #2, featuring in 443 bios",
+		"Jesus was #3, featuring in 363 bios",
+		"Christian was #4, featuring in 360 bios",
+		"Conservative was #5, featuring in 363 bios",
+		"Right was #6, featuring in 91 bios",
+		"St was #7, featuring in 85 bios",
+		"Student was #8, featuring in 79 bios",
+		"Loves was #9-10, featuring in 74 bios",
+		"Good was #9-10, featuring in 74 bios",
+		"Ireland featured in 73 bios",
+		"Man featured in 71 bios",
+		"Born featured in 73 bios",
+		"Make featured in 56 bios",
+		"Now featured in 55 bios",
+		"Political featured in 54 bios",
+		"First featured in 48 bios",
+		"Endorsement featured in 48 bios",
+		"Others featured in 39 bios",
+		"Veteran featured in 39 bios",
+		"Peace featured in 35 bios",
+		"Dublin featured in 27 bios",
+		"College featured in 26 bios",
+		"Students featured in 24 bios",
+		"Serving featured in 24 bios",
+		"Rts featured in 724 bios",
+		"Coffee featured in 23 bios",
+		"Website featured in 19 bios",
+		"Enjoy featured in 19 bios",
+		"Grad featured in 18 bios",
+		"Hate featured in 17 bios",
+		"Following featured in 17 bios",
+		"Serve featured in 13 bios",
+		"Serve featured in 13 bios",
+		"Difference featured in 13 bios",
+		"Development featured in 12 bios",
+		"Song featured in 10 bios",
+		"England featured in 10 bios",
+		
+
+
+
+		"etc",
+
+		"etc..."];
+		 
+		   var srcLocation = locations[sel.selectedIndex];         
+		  
+		   if (srcLocation != undefined && srcLocation != "") {      
+		    document.getElementById('CSTemplates').innerHTML = srcLocation;   
+		  } 
+		}
